ad9f2370ddb71a587127f873cb214ceb4d8a191d
Jason Pitts <juniorjr083@gmail.com>
2018-07-24 17:25:41 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,3 @@
+"CREATE TABLE projects(id INTEGER PRIMARY KEY,title TEXT, category TEXT, fundingoal INTEGER, startdate DATE, enddate DATE)";
+
+"CREATE TABLE users(id INTEGER PRIMARY KEY,name TEXT, age INTEGER)";
